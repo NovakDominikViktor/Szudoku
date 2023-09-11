7c20198a3031844ab84c4a2ad7f8e47f172437f6
Novák Dominik Viktor <ndominikviktor@mitiszkad.lan>
2023-09-11 11:39:42 +0200

    task 3 success

--- sudokuCLI/sudokuCLI/Program.cs
+++ sudokuCLI/sudokuCLI/Program.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace sudokuCLI
 {
     internal class Program
     {
+       
         static void Main(string[] args)
         {
+            List<Feladvany> feladvanyok = new List<Feladvany>();
+
+            try
+            {
+                using (StreamReader reader = new StreamReader("feladvanyok.txt"))
+                {
+                    string sor;
+                    
+                    while((sor = reader.ReadLine()) != null)
+                    {
+                        feladvanyok.Add(new Feladvany(sor));
+                    }
+                }
+            
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine($"3. feladat: Beolvasva {feladvanyok.Count} feladvány ");
+
+            Console.ReadKey();
         }
     }
 }
